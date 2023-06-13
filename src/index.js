4407e92e3e5a072ef7fe93455953701a6ce3a1b4
Shabi Adee <101591738+Shabiblac@users.noreply.github.com>
2023-06-13 14:38:15 +0100

    Add the src folder

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import jokes from "./test";
+import './styles/main.scss';
+
+console.log(jokes());
---
+++ src/test.js
@@ -0,0 +1,6 @@
+const jokes = ()=> {
+    return "Why do programmers prefer dark mode? Because the light attracts bugs!"
+}
+console.log(jokes);
+
+export default jokes;
